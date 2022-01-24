dd97891543b39419a078a3280b41e85e33aa9ef3
seochan99 <gmlcks0513@naver.com>
2022-01-25 01:01:41 +0900

    2

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.navbar__toggleBtn');
+const menu = document.querySelector('.navbar__menu');
+const sns = document.querySelector('.navbar__sns');
+
+toggleBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        sns.classList.toggle('active');
+});
